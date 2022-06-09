7754e034f38876a353034891980028a591225b20
Adam Pultz Melbye <mail@adampultz.com>
2022-06-09 20:38:48 +0200

    Updated Utilities

--- Utilities.hpp
+++ Utilities.hpp
@@ -8,14 +8,10 @@
 #ifndef Utilities_hpp
 #define Utilities_hpp
 
-//#include <base.hpp>
-//#include <literals.hpp>
-//#include <decibel.hpp>
 #include "db_lookup_table.hpp"
 #include "fundamentals.hpp"
 
 using namespace pultzLib;
-using namespace std;
 
 class level_crossfade
 {
@@ -29,9 +25,9 @@ public:
     float process(float a, float b, float ctrl){
         if (ctrl < thresh_)
               {
-                 auto xfade = float(ctrl - thresh_);
+                 float xfade = ctrl - thresh_;
                   xfade = db2a(xfade);
-                 return xfade * a + (1.0 - xfade) * b;
+                 return (xfade * a) + ((1.0f - xfade) * b);
               }
               return a;
     }
@@ -40,31 +36,113 @@ private:
     float thresh_;
 };
 
+class Schmitt_Trigger
+{
+public:
+    Schmitt_Trigger(){};
+    
+    void init(float upperThresh, float lowerThresh){
+        upperThresh_ = upperThresh;
+        lowerThresh_ = lowerThresh;
+    };
+    
+    float process(float s){
+        if (s >= upperThresh_ && state_ == 1) {
+                 state_ = -1;
+              } else if (s < lowerThresh_ && state_ == -1) {
+              state_ = 1;
+              }
+              return state_;
+              }
+    
+private:
+    float upperThresh_;
+    float lowerThresh_;
+    int state_ = 1;
+};
+
+template <class T>
+class Variance
+{
+public:
+    
+    Variance(int size){
+    	size_ = size;
+        sizeMinOne_ = size_ - 1;
+        size_reciproc_ = (float) 1 / size_;
+        values_.resize(size_);
+    };
+    
+    void push(T val, int index){
+        values_[index] = val;
+        sum_ += val;
+    }
+    
+    float process(){ // Calculate mean and variance
+        
+        float mean = sum_ * size_reciproc_;
+        float runningSum = 0;
+        
+        for (int n = 0; n < size_; n++){
+            runningSum += powf(values_[n] - mean, 2);
+        }
+        
+        variance_ = runningSum * size_reciproc_;
+       
+        sum_ = 0;
+        
+        return variance_;
+    }
+    
+    float process(float meanVal){ //If mean is already available, calculate only variance
+        float mean = meanVal;
+        float runningSum = 0;
+        
+        for (int n = 0; n < size_; n++){
+            runningSum += powf(values_[n] - mean, 2);
+        }
+        
+        variance_ = runningSum * size_reciproc_;
+        
+        sum_ = 0;
+        
+        return variance_;
+    }
+    
+private:
+    
+    float variance_;
+    int size_;
+    int sizeMinOne_;
+    float size_reciproc_;
+    vector<float> values_;
+    float sum_;
+};
+
 template <class T>
 class Zero_Crossing
 {
 public:
     Zero_Crossing(){};
     
-    int process(T val){
-        int sign_ = sgn(val * x_m1);
-            if (sign_ == -1){
+    bool process(T val){
+    	unsigned int sign_ = sgn(val * x_m1);
+        if (sign_ == 1){
+            x_m1 = val;
+            return 0;
+        } else if (sign_ == -1 || 0) {
             x_m1 = val;
             return 1;
-            } else {
-                if (val == 0){
-                    x_m1 = val;
-                    return 1;
-                } else {
-                    x_m1 = val;
-                    return 0;
-                }
         }
     }
     
 private:
     T x_m1;
 };
+
+static inline float Balance2(float val_a, float val_b, float balance) {
+	return (val_a * sqrtf(1.0f - balance)) + (val_b * sqrtf(balance));
+}
 
 
 #endif /* Utilities_hpp */
