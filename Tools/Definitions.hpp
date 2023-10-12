f60b774cbc55950d6900981b4f937b65de33836f
Adam Pultz Melbye <mail@adampultz.com>
2023-10-12 14:33:04 +0000

    First commit

---
+++ Tools/Definitions.hpp
@@ -0,0 +1,12 @@
+//Header file for giving access to definitions
+
+#ifndef Definitions_hpp
+#define Definitions_hpp
+
+#define numInputChannels 4;
+#define numOutputChannels 4;
+//Uncomment the below line if you are using the Bela rather a laptop
+
+//#define Bela ;
+
+#endif /* Definitions_hpp */
