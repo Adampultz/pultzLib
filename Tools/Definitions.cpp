28d00ee63855a9319f4230dd22b4b5f25a6be91c
Adam Pultz Melbye <mail@adampultz.com>
2023-11-22 10:38:53 +0000

    Initial commit

---
+++ Tools/Definitions.cpp
@@ -0,0 +1,14 @@
+//
+//  Definitions.cpp
+//  Basic_DSP - App
+//
+//  Created by Adam Pultz Melbye on 20/11/2023.
+//
+
+#include "Definitions.hpp"
+
+double g_SampleRate;
+double g_SampleDur;
+double g_Nyquist;
+int g_numInputs;
+int g_numOutputs;
